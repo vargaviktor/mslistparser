4dc6eb9ea7ca72d9dc739afb97ef94a5b111c933
vargaviktor <viktor.varga@gmail.com>
2022-04-19 13:42:19 +0200

    Added date things

--- mstl.sh
+++ mstl.sh
@@ -10,17 +10,20 @@ echo "(i) Converting STL to ASN1 indented short dump"
 openssl asn1parse -i -oid msoids.txt -in authroot.stl -inform DER | sed 's/.*prim: //; s/.*cons: //' > asn1short.txt
 echo "(i) Writing out full asn1 list"
 openssl asn1parse -i -oid msoids.txt -in authroot.stl -inform DER | sed 's/.*:         OCTET STRING      \[HEX DUMP\]:/Thumbprint: /'  > asn1full.txt
-echo "(i) Extracting thumprints from short ASN1list (based on asn1short file)"
-cat asn1short.txt | grep "OCTET STRING" | sed 's/^        OCTET STRING      \[HEX DUMP\]:/Thumbprint:/' | grep Thumbprint | sed 's/Thumbprint://' > thumbprints.txt
+mstldate="$(cat asn1full.txt | grep -A 2 "MS CTL Root list signer" | grep UTCTIME | sed 's/.*UTCTIME           ://')"
+echo "(i) Date of the list: $mstldate"
 
-#need to add date to thumprint file
+
+echo "(i) Extracting thumbprints from short ASN1list (based on asn1short file)"
+cat asn1short.txt | grep "OCTET STRING" | sed 's/^        OCTET STRING      \[HEX DUMP\]:/Thumbprint:/' | grep Thumbprint | sed 's/Thumbprint://' > thumbprints-$mstldate.txt
+
 
 echo "(i) Downloading certs"
-input="thumbprints.txt"
+input="thumbprints-$mstldate.txt"
 while IFS= read -r line
 do
     echo "(i) Downloading $line"
-#    curl http://ctldl.windowsupdate.com/msdownload/update/v3/static/trustedr/en/$line.crt -o ./certs/$line.crt 
+    curl http://ctldl.windowsupdate.com/msdownload/update/v3/static/trustedr/en/$line.crt -o ./certs/$line.crt 
 done < "$input"
 
 #ez vszinu nem kell
@@ -55,7 +58,7 @@ do
 	cat clean.tmp.eku >> results.txt
     fi
 
-
+cp results.txt results-$mstldate.txt
 
 done < "$input"
 
